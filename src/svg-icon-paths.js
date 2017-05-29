4986c6d7b8845e8b52096caab2d44c790058ad97
Philip Graham <philip.graham@accenture.com>
2017-05-29 13:23:19 -0400

    Remove PathIcon suffix from exported names

--- src/svg-icon-paths.js
+++ src/svg-icon-paths.js
@@ -2,24 +2,24 @@
  * Top level exports. Recommended to only require cherry picked paths.
  */
 
-import addIconPath from "./path/add";
-import bookIconPath from "./path/book";
-import bugIconPath from "./path/bug";
-import checkIconPath from "./path/check";
-import curvedArrowIconPath from "./path/curvedArrow";
-import deleteIconPath from "./path/delete";
-import editIconPath from "./path/edit";
-import technicalDebtIconPath from "./path/technicalDebt";
+import add from "./path/add";
+import book from "./path/book";
+import bug from "./path/bug";
+import check from "./path/check";
+import curvedArrow from "./path/curvedArrow";
+import crossMark from "./path/crossMark";
+import edit from "./path/edit";
+import technicalDebt from "./path/technicalDebt";
 import trashCan from "./path/trashCan";
 
 export {
-	addIconPath,
-	bookIconPath,
-	bugIconPath,
-	checkIconPath,
-	curvedArrowIconPath,
-	deleteIconPath,
-	editIconPath,
-	technicalDebtIconPath,
+	add,
+	book,
+	bug,
+	check,
+	curvedArrow,
+	crossMark,
+	edit,
+	technicalDebt,
 	trashCan
 };
