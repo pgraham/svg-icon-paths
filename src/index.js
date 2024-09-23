6d8063a3e1b9f7e3560b9f26269d91afc02c2eee
Philip Graham <philip.robert.graham@gmail.com>
2024-09-23 02:41:52 +0000

    add reveal icon to exports and to sample doc

--- src/index.js
+++ src/index.js
@@ -2,28 +2,30 @@
  * Top level exports. Recommended to only require cherry picked paths.
  */
 
-import add from "./add";
-import book from "./book";
-import bug from "./bug";
-import check from "./check";
-import curvedArrow from "./curvedArrow";
-import crossMark from "./crossMark";
-import edit from "./edit";
-import leftArrow from "./leftArrow";
-import menu from "./menu";
-import technicalDebt from "./technicalDebt";
-import trashCan from "./trashCan";
+import add from './add'
+import book from './book'
+import bug from './bug'
+import check from './check'
+import curvedArrow from './curvedArrow'
+import crossMark from './crossMark'
+import edit from './edit'
+import leftArrow from './leftArrow'
+import menu from './menu'
+import technicalDebt from './technicalDebt'
+import trashCan from './trashCan'
+import reveal from './reveal'
 
 export {
-	add,
-	book,
-	bug,
-	check,
-	curvedArrow,
-	crossMark,
-	edit,
-	leftArrow,
-	menu,
-	technicalDebt,
-	trashCan
-};
+  add,
+  book,
+  bug,
+  check,
+  curvedArrow,
+  crossMark,
+  edit,
+  leftArrow,
+  menu,
+  technicalDebt,
+  trashCan,
+  reveal,
+}
